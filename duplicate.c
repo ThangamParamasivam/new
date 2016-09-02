278d52a49588bee5a5da13b9038edf22c9d4207c
ThangamParamasivam <thangamparamasivam31896@gmail.com>
2016-09-02 19:08:18 +0530

    Create duplicate.c

---
+++ duplicate.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char *removeDuplicate(char str[])
+{
+int i=0,j;
+char ch;
+printf("\nstr is %s",str);
+while((chstr[i++]!='\0')
+{
+j=i;
+printf("ch=%c",ch);
+if(ch==str[j])
+{
+printf("Yes");
+while(str[j]!='\0')
+printf("%c",str[j]);
+--i;
+}
+break;
+}
+printf("No");
+j++;
+}
+}
+return str;
+}
