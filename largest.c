211545a16a11a0b11683c47b0947c12f36ce4a33
ThangamParamasivam <thangamparamasivam31896@gmail.com>
2016-09-02 12:15:37 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=10,b=8,c=5;
+if((a>b)&&(a>c))
+{
+printf("a");
+}
+else if((b>c))
+{
+printf("b");
+}
+else
+{
+printf("c");
+}
+getch();
+}
