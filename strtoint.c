d19e16a973b15e076deeed01338b8721d9565d38
ThangamParamasivam <thangamparamasivam31896@gmail.com>
2016-09-07 12:08:32 +0530

    Create strtoint.c

---
+++ strtoint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n[100];
+int dec=0,i,j;
+pprintf("Enter the number");
+gets(n);
+len=strlen(n);
+for(i=0;i<len;i++)
+dec=dec*10+(n[i]-'0');
+}
+printf("dec");
+getch();
+}
+
